7880545032f907779d11e9462185f95e8cb21161
Jafet <moranorozcokevinjafet@gmail.com>
2024-07-18 09:23:33 -0600

    compare score vs accuracy

--- practicasTensorFlow/genetic_algorithm.py
+++ practicasTensorFlow/genetic_algorithm.py
@@ -190,6 +190,6 @@ if __name__ == '__main__':
 
 
   # create the genetic algorithm
-  ga = GeneticAlgorithm(6, 4, 100, 5, 0.05)
+  ga = GeneticAlgorithm(4, 4, 100, 6, 0.15)
   # run the genetic algorithm
   ga.run(X_train, y_train, X_test, y_test, 10)
